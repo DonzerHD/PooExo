cc21145f78c5aaece2c5c061018d2af791d8f62c
Thomas.l59 <thomas.lemay59@outlook.fr>
2022-11-18 19:59:16 +0100

    Second commit

--- src/exoPOO/CompteBancaire.java
+++ src/exoPOO/CompteBancaire.java
@@ -2,19 +2,50 @@ package exoPOO;
 
 public class CompteBancaire {
 	// attributs
-      
-
+      double solde;
+        
     
     //Constructeur
-   
-    
+     public CompteBancaire(double solde) { 
+    	  this.solde = solde;
+      }
+
     //méthodes
- 
-	public static void Indication(final String intitulé, final double montant) {
-		   if (montant > 0 ) {
-			   System.out.println(intitulé + " +" + montant + "€"); 
-		   }else{
-		   System.out.println(intitulé + " " + montant + "€");
-		   }
-	}
+     
+     public String CalculSolde(double ligne1, double ligne2, double ligne3, double ligne4, double ligne5) {
+    	double sommeFinal = this.solde + ligne1 + ligne2 + ligne3 + ligne4 + ligne5;
+    	 return "Le solde de votre compte est de : " + sommeFinal + "€";
+     }
+     
+     public String CalculMoyenneLigne(double ligne1, double ligne2, double ligne3, double ligne4, double ligne5) {
+    	 double sommeFinal = (ligne1 + ligne2 + ligne3 + ligne4 + ligne5) / 5;
+    	 return "La moyenne des lignes de compte est de : " + sommeFinal + "€";
+     }
+     
+     public void  LigneE(double ligne1, double ligne2, double ligne3, double ligne4, double ligne5) {
+    	 if( ligne1 >= ligne2 && ligne1 >= ligne3 && ligne1 >= ligne4 && ligne1 >= ligne5)
+             System.out.println("La ligne la plus élevée est "+ ligne1);
+         else if (ligne2 >= ligne1 && ligne2 >= ligne3 && ligne2 >= ligne4 && ligne2 >= ligne5)
+             System.out.println("La ligne la plus élevée est "+ ligne2);
+         else if (ligne3 >= ligne1 && ligne3 >= ligne2 && ligne3 >= ligne4 && ligne3 >= ligne5)
+             System.out.println("La ligne la plus élevée est "+ ligne3);
+         else if (ligne4 >= ligne1 && ligne4 >= ligne2 && ligne4 >= ligne3 && ligne4 >= ligne5)
+             System.out.println("La ligne la plus élevée est "+ ligne4);
+         else
+             System.out.println("La ligne la plus élevée est " + ligne5 + "€");
+     }
+     
+     public void  LigneF(double ligne1, double ligne2, double ligne3, double ligne4, double ligne5) {
+    	 if( ligne1 <= ligne2 && ligne1 <= ligne3 && ligne1 <= ligne4 && ligne1 <= ligne5)
+             System.out.println("La ligne la plus faible est "+ ligne1);
+         else if (ligne2 <= ligne1 && ligne2 <= ligne3 && ligne2 <= ligne4 && ligne2 <= ligne5)
+             System.out.println("La ligne la plus faible est "+ ligne2);
+         else if (ligne3 <= ligne1 && ligne3 <= ligne2 && ligne3 <= ligne4 && ligne3 <= ligne5)
+             System.out.println("La ligne la plus faible est "+ ligne3);
+         else if (ligne4 <= ligne1 && ligne4 <= ligne2 && ligne4 <= ligne3 && ligne4 <= ligne5)
+             System.out.println("La ligne la plus faible est "+ ligne4);
+         else
+             System.out.println("La ligne la plus faible est " + ligne5 + "€");
+     }
 }
+
--- src/exoPOO/Programme.java
+++ src/exoPOO/Programme.java
@@ -17,19 +17,23 @@ public class Programme {
 	personne3.AffichageInfo(); */
 	
 	 //Comptes Bancaires
-  
-	  LigneCompteBancaire ligne1 = new LigneCompteBancaire("Carrefour", 12.44);
-	  System.out.println(ligne1.Indication());
-	  
-	  
-	
+  	 	 LigneCompteBancaire ligne1 = new LigneCompteBancaire("Carrefour", -64.45);
+  	 	 LigneCompteBancaire ligne2 = new LigneCompteBancaire("Micromania", -32.55);
+  	 	 LigneCompteBancaire ligne3 = new LigneCompteBancaire("Salaire", 2264.45);
+  	 	 LigneCompteBancaire ligne4 = new LigneCompteBancaire("Crabe", 23.44);
+  	 	 LigneCompteBancaire ligne5 = new LigneCompteBancaire("Trade", 22.33);
+  	 	  
+  	 	System.out.println(ligne1.Indication());
+  	 	System.out.println(ligne2.Indication());
+  	 	System.out.println(ligne3.Indication());
+  	 	System.out.println(ligne4.Indication());
+  	 	System.out.println(ligne5.Indication());
+  	 	
+  	 	CompteBancaire compte1 = new CompteBancaire(300.44);
+  	 	System.out.println(compte1.CalculSolde(ligne1.montant, ligne2.montant, ligne3.montant, ligne4.montant, ligne5.montant));
+  	 	System.out.println(compte1.CalculMoyenneLigne(ligne1.montant, ligne2.montant, ligne3.montant, ligne4.montant, ligne5.montant));
+  	 	compte1.LigneE(ligne1.montant, ligne2.montant, ligne3.montant, ligne4.montant, ligne5.montant);
+  	 	compte1.LigneF(ligne1.montant, ligne2.montant, ligne3.montant, ligne4.montant, ligne5.montant);
+         
 	  }	
-  
-  public static void Indication(final String intitulé, final double montant) {
-	   if (montant > 0 ) {
-		   System.out.println(intitulé + " +" + montant + "€"); 
-	   }else{
-	   System.out.println(intitulé + " " + montant + "€");
-	   }
-}
 }
